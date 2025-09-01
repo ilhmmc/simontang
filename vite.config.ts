5f5174ff5826ca569214bec9dac1831c91fd2a32
222212756 <222212756@stis.ac.id>
2025-09-01 15:44:55 +0700

    Try to deploy vercel again

--- vite.config.ts
+++ vite.config.ts
@@ -23,7 +23,7 @@ export default defineConfig(({ mode }) => ({
       "@shared": path.resolve(__dirname, "./shared"),
     },
   },
-  base: process.env.NODE_ENV || "/simontang",
+  base: process.env.NODE_ENV || "/semar-mesem",
 }));
 
 function expressPlugin(): Plugin {
